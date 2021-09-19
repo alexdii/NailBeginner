349eeb29e65e82feb7fdaa2db16e45edd6dbc2d9
alexdii <alexdii4000@gmail.com>
2021-09-20 02:48:22 +0300

    Initial commit

---
+++ src/Less4_While.java
@@ -0,0 +1,11 @@
+public class Less4_While {
+    public static void main(String[] args) {
+        int value = 0;
+        while (value<5) {
+            System.out.println("Hello " + value);
+            value++;
+        }
+        // в цикле while нужен какой-нибудь логический оператор - до каких пор будет выполняться условие
+        // while - это цикл, который выполняется до тех пор, пока условие в круглых скобках дает true
+    }
+}
